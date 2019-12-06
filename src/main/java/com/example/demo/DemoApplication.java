2a755f4aeec6f10f7f01a234b378258ca27473ae
wugithub01 <44189942+wugithub01@users.noreply.github.com>
2019-12-06 08:34:27 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//hello
+//hellosssss
+//asadasdsad
 @SpringBootApplication
 public class DemoApplication {
 
